b407dc706b108131dcf32ca16103b4979ea1f65e
Matt Brender <mjbrender@gmail.com>
2015-03-31 17:13:26 -0400

    printing out all users who star these repos with pagination turned off for testing faster

--- star_hunting.rb
+++ star_hunting.rb
@@ -1,16 +1,26 @@
 require 'github_api'
 load "pw.config"
 
+# Authentication 
 github = Github.new login:'mjbrender', password: PW
-list = github.repos.list public: true, org: 'basho-labs', auto_pagination: true
 
-list.each do |repo|
-    puts repo.name
-end
+# get all repositories visible under basho-labs
+# note that it includes public and private
+list = github.repos.list org: 'basho-labs', auto_pagination: true
 
+# hash that will contain all unique users 
+all_users = {}
 
 # for all repos 
 # if key doesn't exist
 # put username as value 
 # print size of table 
 
+list.each do |repo|
+    # Make the call to get all users from the repo
+    all_stars = github.activity.starring.list user: 'basho-labs', repo: repo.name #, auto_pagination: true
+
+    all_stars.each do |user|
+        puts user.login
+    end
+end
